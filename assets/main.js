91d39f33c5829ccc7a50a1bbc470ebfe636a6180
Jhonathan Pimenta <jhonathanscp@gmail.com>
2024-01-16 19:48:42 -0300

    logic prototype

--- assets/main.js
+++ assets/main.js
@@ -1,37 +1,55 @@
+// Autor: Jhonathan Pimenta
+// Cada bloquinho de tela possui 25px, então o left e top tem que ser multiplos de 25px.
+// Quanto menor snake.speed mais rápido a serpente fia.
 function Game() {
   this.snake = {
     element: document.querySelector('#snake'),
-    left: 0,
-    top: 0,
+    speed: 350,
+    left: 250,
+    top: 250,
     setInitialPosition() {
-      this.element.style.left = this.left + '%';
-      this.element.style.top = this.top + '%';
+      this.element.style.left = this.left + 'px';
+      this.element.style.top = this.top + 'px';
     }
   };
 
   this.fruit = {
     element: document.querySelector('#fruit'),
-    left: 10,
-    top: 0,
+    left: 25,
+    top: 100,
     setInitialPosition() {
-      this.element.style.left = this.left + '%';
-      this.element.style.top = this.top + '%';
+      this.element.style.left = this.left + 'px';
+      this.element.style.top = this.top + 'px';
     }
   }
   this.snake.setInitialPosition()
   this.fruit.setInitialPosition()
   window.addEventListener('keydown', (e) => this.move(e))
 
-
   this.move = (event) => {
-    const key = event.key;
-    console.log(key)
-      if((key === 'w') || (key == 'ArrowUp')) this.snake.element.style.top = `${this.snake.top -= 0.5}%`;
-      if((key === 'a') || (key === 'ArrowLeft')) this.snake.element.style.left = `${this.snake.left -= 0.5}%`;
-      if((key === 's') || (key === 'ArrowDown')) this.snake.element.style.top = `${this.snake.top += 0.5}%`;
-      if((key === 'd') || (key === 'ArrowRight')) this.snake.element.style.left = `${this.snake.left += 0.5}%`;
+    if (event.key === this.lastKey) return;
+    this.lastKey = event.key;
+    clearInterval(this.interval);
 
-      if((this.snake.left === this.fruit.left) && (this.snake.top === this.fruit.top)) this.aumenta
+    if ((this.lastKey === 'w') || (this.lastKey == 'ArrowUp')) {
+      this.snake.element.style.top = `${this.snake.top -= 25}px`
+      this.interval = setInterval(() => { this.snake.element.style.top = `${this.snake.top -= 25}px` }, this.snake.speed);
+    }
+    if ((this.lastKey === 'a') || (this.lastKey === 'ArrowLeft')) {
+      this.snake.element.style.left = `${this.snake.left -= 25}px`
+      this.interval = setInterval(() => { this.snake.element.style.left = `${this.snake.left -= 25}px` }, this.snake.speed);
+    }
+    if ((this.lastKey === 's') || (this.lastKey === 'ArrowDown')) {
+      this.snake.element.style.top = `${this.snake.top += 25}px`
+      this.interval = setInterval(() => { this.snake.element.style.top = `${this.snake.top += 25}px` }, this.snake.speed);
+    }
+    if ((this.lastKey === 'd') || (this.lastKey === 'ArrowRight')) {
+      this.snake.element.style.left = `${this.snake.left += 25}px`
+      this.interval = setInterval(() => { this.snake.element.style.left = `${this.snake.left += 25}px` }, this.snake.speed);
+    }
+
   }
+
+
 }
 const game = new Game();
